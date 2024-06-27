49860ed3de4a50a021f18590443953753aa7ab99
whoiam1101 <whoiam1101@outlook.com>
2024-06-27 14:15:15 +0300

    bug fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
 import asyncio
-import whisper
 
+from pathlib import Path
 from uuid import uuid4
 from openai import OpenAI
-from aiogram import Bot, types, Dispatcher
+from aiogram import Bot, types, Dispatcher, F
+from aiogram.filters import Command
 from aiogram.types import Message, FSInputFile
 from pydantic_settings import BaseSettings
 
@@ -17,8 +19,6 @@ dp = Dispatcher()
 config = Config.parse_file('.env')
 
 bot = Bot(token=config.telegram_bot_token)
-
-audio2text_model = whisper.load_model("base")
 
 client = OpenAI(api_key=config.openai_api_key)
 assistant = client.beta.assistants.create(
@@ -38,16 +38,16 @@ async def get_audio(message: types.message) -> str:
     return file_name
 
 
-def audio2text(file_name: str) -> str:
-    audio = whisper.load_audio(file_name)
-    audio = whisper.pad_or_trim(audio)
-    mel = whisper.log_mel_spectrogram(audio).to(audio2text_model.device)
-    options = whisper.DecodingOptions()
-    result = whisper.decode(audio2text_model, mel, options)
-    return result.text
+async def audio2text(file_name: str) -> str:
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=file_name,
+        response_format='text'
+    )
+    return transcript.text
 
 
-def get_answer(text: str) -> str:
+async def get_answer(text: str) -> str:
     thread = client.beta.threads.create()
     client.beta.threads.messages.create(
         thread_id=thread.id,
@@ -58,16 +58,16 @@ def get_answer(text: str) -> str:
         thread_id=thread.id,
         assistant_id=assistant.id,
     )
+    client.beta.threads.runs.create_and_poll()
     run = client.beta.threads.runs.retrieve(
         thread_id=thread.id,
         run_id=run.id,
     )
     messages = client.beta.threads.messages.list(thread_id=thread.id)
-    # print(f"role: {messages.data[0].role}\t content: {messages.data[0].content[]}")
     return messages.data[0].content[0].text.value
 
 
-def text2audio(text: str) -> str:
+async def text2audio(text: str) -> str:
     audio_file_name = f"audio/{str(uuid4())}.mp3"
     response = client.audio.speech.create(
         model="tts-1",
@@ -78,19 +78,20 @@ def text2audio(text: str) -> str:
     return audio_file_name
 
 
-@dp.message()
+@dp.message(Command("start"))
 async def cmd_start(message: Message):
     audio_file_name = await get_audio(message)
-    text = audio2text(audio_file_name)
-    answer = get_answer(text)
-    audio_file_name = text2audio(answer)
+    text = await audio2text(audio_file_name)
+    Path(audio_file_name).unlink()  # remove file
+    answer = await get_answer(text)
+    audio_file_name = await text2audio(answer)
     audio_file = FSInputFile(audio_file_name)
     await message.reply_voice(audio_file)
+    Path(audio_file_name).unlink()
 
 
 async def main() -> None:
-    from sys import stderr
-    print("BOT Start", file=stderr)
+    print("BOT Start", file=sys.stderr)
     await dp.start_polling(bot)
 
 
@@ -98,4 +99,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("BOT Closed")
+        print("BOT Closed", file=sys.stderr)
